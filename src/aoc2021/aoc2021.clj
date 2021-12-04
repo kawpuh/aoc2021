c6cd415daecc201e08deb6343c77b75b06a7baa8
ethan <e.r.sorrell@gmail.com>
2021-12-04 15:27:59 -0600

    day 2

--- src/aoc2021/aoc2021.clj
+++ src/aoc2021/aoc2021.clj
@@ -19,8 +19,45 @@
                  (count))]
     (println ans)))
 
+(defn day2-part1 []
+  (let [input (slurp "day2.txt")
+        cmds (->> input s/split-lines
+                  (map #(s/split % #"\s"))
+                  (map #(vector (keyword (first %))
+                                (Integer/parseInt (second %)))))
+        [final-horiz final-depth]
+        (reduce
+          (fn [[horiz depth] [instr n]]
+            (case instr
+              :forward [(+ n horiz) depth]
+              :down [horiz (+ depth n)]
+              :up [horiz (- depth n)]))
+          [0 0]
+          cmds)]
+    (* final-horiz final-depth)))
+
+(defn day2-part2 []
+  (let [input (slurp "day2.txt")
+        cmds (->> input s/split-lines
+                  (map #(s/split % #"\s"))
+                  (map #(vector (keyword (first %))
+                                (Integer/parseInt (second %)))))
+        [final-horiz final-depth _final-aim]
+        (reduce
+          (fn [[horiz depth aim] [instr n]]
+            (case instr
+              :forward [(+ n horiz) (+ depth (* n aim)) aim]
+              :down [horiz depth (+ aim n)]
+              :up [horiz depth (- aim n)]))
+          [0 0 0]
+          cmds)]
+    (* final-horiz final-depth)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (day1-part1)
-  (day1-part2))
+  ; (day1-part1)
+  ; (day1-part2)
+  (println (day2-part1))
+  (println (day2-part2))
+  )
